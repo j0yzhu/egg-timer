b4cec070cb5dde0493775f50a769ff7ff2186b5a
Joy <xiaominzhu15@gmail.com>
2025-02-23 04:21:47 +1300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import {useEffect, useState} from 'react'
+import './App.css'
+
+function App() {
+    const [timer, setTimer] = useState<number | undefined>(undefined);
+    const [clicked, setClicked] = useState<boolean>(false);
+
+    function handleClick(time: number) {
+        setClicked(true);
+        setTimer(time)
+    }
+
+    const minutes = timer === undefined ? 0 : Math.floor(timer / 60);
+    const seconds = timer === undefined ? 0 : timer % 60;
+
+    useEffect(() => {
+        if (timer === 0) {
+            return;
+        }
+
+        const interval = setInterval(() => setTimer(timer - 1), 1000)
+        return () => clearInterval(interval)
+
+    })
+
+    return (
+        <>
+            {
+                !clicked &&
+                <div>
+                    <button onClick={() => handleClick(3)}>Soft Boiled (3min)</button>
+                    <button onClick={() => handleClick(5 * 60)}>Medium Boiled (5min)</button>
+                    <button onClick={() => handleClick(7 * 60)}>Hard Boiled (7min)</button>
+                    <button onClick={() => handleClick(10 * 60)}>Extra Hard Boiled (10min)</button>
+                </div>
+            }
+            {
+                clicked &&
+                <div>
+                     <p>{minutes}:{seconds < 10 && 0}{seconds}</p>
+                </div>
+            }
+            {
+                timer === 0 && <h1>Your eggs are DONE!</h1>
+            }
+        </>
+    )
+}
+
+export default App
