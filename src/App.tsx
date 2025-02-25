bfa9526d61edc4fcdefa642289f07f2b2101831f
Joy <xiaominzhu15@gmail.com>
2025-02-26 01:04:10 +1300

    css to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import {useEffect, useState} from 'react'
-import './App.css'
+import {ReactNode, useEffect, useState} from 'react'
 
 function App() {
     const [timer, setTimer] = useState<number | undefined>(undefined);
     const [clicked, setClicked] = useState<boolean>(false);
 
     function handleClick(time: number) {
+        console.log(time);
+        console.log("clicked")
         setClicked(true);
         setTimer(time)
     }
@@ -24,27 +25,43 @@ function App() {
     })
 
     return (
-        <>
+        <div className={"display-flex"}>
             {
                 !clicked &&
-                <div>
-                    <button onClick={() => handleClick(3)}>Soft Boiled (3min)</button>
-                    <button onClick={() => handleClick(5 * 60)}>Medium Boiled (5min)</button>
-                    <button onClick={() => handleClick(7 * 60)}>Hard Boiled (7min)</button>
-                    <button onClick={() => handleClick(10 * 60)}>Extra Hard Boiled (10min)</button>
+                <div className="flex flex-col items-center gap-2">
+                    <MyButton onClick={() => handleClick(3)}>Soft Boiled (3min)</MyButton>
+                    <MyButton onClick={() => handleClick(3)}>Medium Boiled (5min)</MyButton>
+                    <MyButton onClick={() => handleClick(3)}>Hard bBoiled (7min)</MyButton>
+                    <MyButton onClick={() => handleClick(3)}>Extra Hard Boiled (10min)</MyButton>
                 </div>
             }
             {
                 clicked &&
                 <div>
                     <p>Your eggs are ready in...</p>
-                     <p>{minutes}:{seconds < 10 && 0}{seconds}</p>
+                    <p>{minutes}:{seconds < 10 && 0}{seconds}</p>
                 </div>
             }
             {
                 timer === 0 && <h1>All done, enjoy your eggs!</h1>
             }
-        </>
+        </div>
+    )
+}
+
+interface MyButtonProps {
+    children: ReactNode
+    onClick: () => void
+}
+
+const MyButton: React.FC<MyButtonProps> = ({children, onClick}) => {
+    return (
+        <button
+            onClick={onClick}
+            className="border-r-8 border-solid px-2 py-4 cursor-pointer transition-transform duration-300
+            hover:border-white block bg-amber-100 max-w-sm">
+            {children}
+        </button>
     )
 }
 
