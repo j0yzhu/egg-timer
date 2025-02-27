d7594be3f73e0fdd32a064304cc801e93497c0e4
Joy <xiaominzhu15@gmail.com>
2025-02-27 20:50:59 +1300

    Add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import {ReactNode, useEffect, useState} from 'react'
+import bgImage from './assets/Chick.png'
+
 
 function App() {
     const [timer, setTimer] = useState<number | undefined>(undefined);
@@ -25,28 +27,32 @@ function App() {
     })
 
     return (
-        <div className="flex justify-center">
-            <div className="relative flex flex-col items-center justify-center bg-[#d6a478] w-[384px] h-[500px]
-            rounded-[50%/60%_60%_40%_40%] drop-shadow-[0px_15px_3px_rgba(0,0,0,0.25)]">
-                <h1 className="absolute top-15 font-serif text-[#905a2c] text-2xl font-bold">Egg Timer</h1>
-                {
-                    !clicked &&
-                    <div className="flex flex-col items-center justify-center gap-2">
-                        <MyButton onClick={() => handleClick(3)}>Soft Boiled (3min)</MyButton>
-                        <MyButton onClick={() => handleClick(5 * 60)}>Medium Boiled (5min)</MyButton>
-                        <MyButton onClick={() => handleClick(7 * 60)}>Hard Boiled (7min)</MyButton>
-                        <MyButton onClick={() => handleClick(10 * 60)}>Extra Hard Boiled (10min)</MyButton>
-                    </div>
-                }
-                {
-                    clicked &&
-                    <div className="flex flex-col items-center justify-center h-screen font-serif text-xl font-medium text-[#6b4320]">
-                        <p>Your eggs are ready in...</p>
-                        <p>{minutes}:{seconds < 10 && 0}{seconds}</p>
-                        {timer === 0 && <h1>Enjoy your eggs!</h1>}
-                        <button></button>
-                    </div>
-                }
+        <div style={{ backgroundImage: `url(${bgImage})` }} className="bg-cover bg-center h-screen">
+            <div className="flex justify-center p-6">
+                <div className="relative flex flex-col items-center justify-center
+                bg-gradient-to-b from-[#e2c3a8] to-[#c28d5c] w-[384px] h-[500px]
+                rounded-[50%/65%_65%_45%_45%] drop-shadow-[0px_25px_10px_rgba(0,0,0,0.3)]">
+                    <h1 className="absolute top-15 font-serif text-[#905a2c] text-2xl font-bold">Egg Timer</h1>
+                    {
+                        !clicked &&
+                        <div className="flex flex-col items-center justify-center gap-2">
+                            <MyButton onClick={() => handleClick(3)}>Soft Boiled (3min)</MyButton>
+                            <MyButton onClick={() => handleClick(5 * 60)}>Medium Boiled (5min)</MyButton>
+                            <MyButton onClick={() => handleClick(7 * 60)}>Hard Boiled (7min)</MyButton>
+                            <MyButton onClick={() => handleClick(10 * 60)}>Extra Hard Boiled (10min)</MyButton>
+                        </div>
+                    }
+                    {
+                        clicked &&
+                        <div
+                            className="flex flex-col items-center justify-center h-screen font-serif text-xl font-medium text-[#6b4320]">
+                            <p>Your eggs are ready in...</p>
+                            <p>{minutes}:{seconds < 10 && 0}{seconds}</p>
+                            {timer === 0 && <h1>Enjoy your eggs!</h1>}
+                            <button></button>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
     )
@@ -57,7 +63,7 @@ interface MyButtonProps {
     onClick: () => void
 }
 
-const MyButton: React.FC<MyButtonProps> = ({ children, onClick }) => {
+const MyButton: React.FC<MyButtonProps> = ({children, onClick}) => {
     return (
         <button
             onClick={onClick}
